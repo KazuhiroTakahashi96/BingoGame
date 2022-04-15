f85e172977bd4297b466c5a942353c548b699cd4
Kazu Takahashi <num96kazu19@icloud.com>
2022-04-15 16:49:44 +0900

    ビンゴカードクラスの作成　要改善

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const reachNum = document.querySelector("#reach-num");
 const bingoNum = document.querySelector("#bingo-num");
 const ballNum = document.querySelector("#ball-num");
+
+// ボタン取得
+const createBtn = document.querySelector("#create-btn");
 const ballBtn = document.querySelector("#ball-btn");
 const resetBtn = document.querySelector("#reset-btn");
 
+// B, I, N, G, O 列の取得
 const col_B = document.querySelectorAll(".col-B");
-console.log(col_B);
-for (let i = 0; i < 5; i++) {
-  col_B[i].innerHTML = Math.floor(Math.random() * 15) + 1;
-}
+const col_I = document.querySelectorAll(".col-I");
+const col_N = document.querySelectorAll(".col-N");
+const col_G = document.querySelectorAll(".col-G");
+const col_O = document.querySelectorAll(".col-O");
 
 //==================== ビンゴカードの作成 ====================
 class BingoCard {
-  constructor(x) {
-    this.sum = Math.floor(Math.random() * x);
-    console.log(this.sum);
-  }
-  calc() {
-    console.log(this.sum * 10);
+  constructor() {
+    console.log(this);
+
+    for (let i = 0; i < 5; i++) {
+      col_B[i].innerHTML = Math.floor(Math.random() * 15) + 1;
+      col_I[i].innerHTML = Math.floor(Math.random() * 15) + 16;
+      col_N[i].innerHTML = Math.floor(Math.random() * 15) + 31;
+      col_G[i].innerHTML = Math.floor(Math.random() * 15) + 46;
+      col_O[i].innerHTML = Math.floor(Math.random() * 15) + 61;
+      // 中央は常に'free'
+      col_N[2].innerHTML = "free";
+    }
   }
 }
 
@@ -29,8 +39,14 @@ class BingoBallNumber {
 }
 const bingoBallNumber = new BingoBallNumber();
 
-////////////////////////
+//======================== ボタンアクション ========================
+// === ビンゴカード作成ボタン ===
+createBtn.addEventListener("click", () => {
+  const card = new BingoCard();
+  console.log(card);
+});
 
+// === ビンゴボールを引くボタン ===
 ballBtn.addEventListener("click", () => {
   let ballNumArray = [];
   for (let i = 1; i <= 75; i++) {
@@ -46,7 +62,7 @@ ballBtn.addEventListener("click", () => {
     console.log("success");
     ballNum.innerHTML = randomNum;
   }
-
-  const card = new BingoCard(100);
-  card.calc();
 });
+
+// === リセットボタン ===
+resetBtn.addEventListener("click", () => {});
