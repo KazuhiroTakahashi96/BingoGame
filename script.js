da62d085def6e35ced4fa9449a81a82164cd76c7
Kazu Takahashi <num96kazu19@icloud.com>
2022-04-15 23:44:24 +0900

    ランダムな数字を画面に出力するのに成功

--- script.js
+++ script.js
@@ -14,21 +14,12 @@ const col_N = document.querySelectorAll(".col-N");
 const col_G = document.querySelectorAll(".col-G");
 const col_O = document.querySelectorAll(".col-O");
 
+// 全td要素（ビンゴカードの数字）の取得
+const td = document.querySelectorAll("td");
+
 //==================== ビンゴカードの作成 ====================
 class BingoCard {
-  constructor() {
-    console.log(this);
-
-    for (let i = 0; i < 5; i++) {
-      col_B[i].innerHTML = Math.floor(Math.random() * 15) + 1;
-      col_I[i].innerHTML = Math.floor(Math.random() * 15) + 16;
-      col_N[i].innerHTML = Math.floor(Math.random() * 15) + 31;
-      col_G[i].innerHTML = Math.floor(Math.random() * 15) + 46;
-      col_O[i].innerHTML = Math.floor(Math.random() * 15) + 61;
-      // 中央は常に'free'
-      col_N[2].innerHTML = "free";
-    }
-  }
+  constructor() {}
 }
 
 //================= ビンゴボール（数字）の取り出し =================
@@ -36,14 +27,20 @@ class BingoBallNumber {
   constructor() {
     console.log("ビンゴボールの取り出し");
   }
+
+  makeClass() {
+    const bingoCaed = new BingoCard();
+    console.log(bingoCaed);
+  }
 }
-const bingoBallNumber = new BingoBallNumber();
 
 //======================== ボタンアクション ========================
 // === ビンゴカード作成ボタン ===
 createBtn.addEventListener("click", () => {
   const card = new BingoCard();
   console.log(card);
+
+  showBingoCard();
 });
 
 // === ビンゴボールを引くボタン ===
@@ -66,3 +63,41 @@ ballBtn.addEventListener("click", () => {
 
 // === リセットボタン ===
 resetBtn.addEventListener("click", () => {});
+
+// =============ビンゴカードの数字を作成する関数============
+function makeRandomNum(plus) {
+  let array = [];
+  for (let i = 0; i < 15; i++) {
+    array.push(i);
+  }
+
+  let numArray = [];
+  for (let i = 0; i < 5; i++) {
+    // ランダムな値を取得
+    let randomNum = Math.floor(Math.random() * array.length);
+    numArray.push(array[randomNum] + plus);
+    // 数字が重複しないよう、元の配列から削除
+    array.splice(randomNum, 1);
+  }
+  return numArray;
+}
+
+// ==================上で作成した数字を出力する関数===================
+function showBingoCard() {
+  const B_Num = makeRandomNum(1);
+  const I_Num = makeRandomNum(16);
+  const N_Num = makeRandomNum(31);
+  const G_Num = makeRandomNum(46);
+  const O_Num = makeRandomNum(61);
+
+  // 数字を画面に出力
+  for (let i = 0; i < 5; i++) {
+    col_B[i].innerHTML = B_Num[i];
+    col_I[i].innerHTML = I_Num[i];
+    col_N[i].innerHTML = N_Num[i];
+    col_G[i].innerHTML = G_Num[i];
+    col_O[i].innerHTML = O_Num[i];
+  }
+  // 中央は常に'free'
+  col_N[2].innerHTML = "free";
+}
